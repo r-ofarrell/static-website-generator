4deb5b7c1110786d7d5ff1b762232eae4da3e831
r-ofarrell <ryanof@pm.me>
2024-11-10 22:40:42 -0500

    Refactored block_to_block and added multiple new tests for it.

--- src/convert_blocks.py
+++ src/convert_blocks.py
@@ -17,7 +17,7 @@ def block_to_block_type(markdown_block):
     lines = markdown_block.split("\n")
     if re.search(r"^#{1,6} \w", markdown_block):
         return "heading"
-    if if len(lines) == 1 and re.search(r"^`{3}.*`{3}$", markdown_block):
+    if len(lines) == 1 and re.search(r"^`{3}.*`{3}$", markdown_block):
         return "code"
     if len(lines) > 1 and re.search(r"^`{3}", lines[0]) and re.search(r"`{3}$", lines[-1]):
         return "code"
@@ -26,15 +26,22 @@ def block_to_block_type(markdown_block):
             if not re.search(r"^>", line):
                 return "paragraph"
         return "quote"
-    if re.search(r"^[-*] ", markdown_block):
+    if re.search(r"^\* ", markdown_block):
         for line in lines:
-            if not re.search(r"^[-*] ", line):
+            if not re.search(r"^\* ", line):
+                return "paragraph"
+        return "unordered_list"
+    if re.search(r"^- ", markdown_block):
+        for line in lines:
+            if not re.search(r"^- ", line):
                 return "paragraph"
         return "unordered_list"
     if re.search(r"^[0-9]+\. ", markdown_block):
+        num = 1
         for line in lines:
-            if not re.search(r"^[0-9]+\. ", line):
+            if not line.startswith(f"{num}. "):
                 return "paragraph"
+            num += 1
         return "ordered_list"
 
     return "paragraph"
---
+++ src/test_convert_blocks.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from convert_blocks import markdown_to_block, block_to_block_type
+
+
+class TestMarkdownToBlock(unittest.TestCase):
+    def test_three_blocks_one_line_separating(self):
+        page = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+        blocks = markdown_to_block(page)
+        expected = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+        ]
+
+        self.assertListEqual(blocks, expected)
+
+    def test_three_blocks_multiple_lines_separating(self):
+        page = """# This is a heading
+
+
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+        blocks = markdown_to_block(page)
+        expected = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+        ]
+
+        self.assertListEqual(blocks, expected)
+
+    def test_three_blocks_with_training_whitespace(self):
+        page = """# This is a heading
+
+
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+        """
+        blocks = markdown_to_block(page)
+        expected = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+            "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+        ]
+
+        self.assertListEqual(blocks, expected)
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        block = "### I am a header"
+        result = block_to_block_type(block)
+        self.assertEqual("heading", result)
+
+    def test_code(self):
+        block = "````I am code\nand more code````"
+        result = block_to_block_type(block)
+        self.assertEqual("code", result)
+
+    def test_multiline_code(self):
+        block = "```I am code\nthat continues\non new lines.```"
+        result = block_to_block_type(block)
+        self.assertEqual("code", result)
+
+    def test_quote(self):
+        block = "> I am a quote\n> With more quote"
+        result = block_to_block_type(block)
+        self.assertEqual("quote", result)
+
+    def test_quote_invalid_markdown(self):
+        block = "> I am a quote.\nWith invalide stuff.\n> And more quote."
+        result = block_to_block_type(block)
+        self.assertEqual("paragraph", result)
+
+    def test_unordered_list(self):
+        block = "* I am an unordered list\n* And another one"
+        result = block_to_block_type(block)
+        self.assertEqual("unordered_list", result)
+
+    def test_unordered_list_with_paragraph(self):
+        block = "* I am an unordered list.\nWith some paragraph text"
+        result = block_to_block_type(block)
+        self.assertEqual("paragraph", result)
+
+    def test_unordered_list_with_invalid_markdown(self):
+        block = "* I am an unordered list.\n- And I like to switch it up."
+        result = block_to_block_type(block)
+        self.assertEqual("paragraph", result)
+
+    def test_ordered_list(self):
+        block = "1. I am an ordered list\n2. Item two"
+        result = block_to_block_type(block)
+        self.assertEqual("ordered_list", result)
+
+    def test_ordered_list_invalid_markdown(self):
+        block = "1. One\n2. Two\n4. Three"
+        result = block_to_block_type(block)
+        self.assertEqual("paragraph", result)
+
+    def test_paragraph(self):
+        block = "I am just a plain ol' paragraph"
+        result = block_to_block_type(block)
+        self.assertEqual("paragraph", result)
