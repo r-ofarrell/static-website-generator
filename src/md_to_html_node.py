3d6bb9d8442cc0eb7edac559ba096fbd763e29fb
r-ofarrell <ryanof@pm.me>
2024-11-11 22:55:51 -0500

    Hopeless changes to file.

--- src/md_to_html_node.py
+++ src/md_to_html_node.py
@@ -2,6 +2,7 @@
 
 import re
 from htmlnode import HTMLNode, ParentNode, LeafNode
+from textnode import text_node_to_html_node
 from split_and_extract import text_to_textnodes
 from convert_blocks import (
     markdown_to_block,
@@ -17,43 +18,64 @@ from convert_blocks import (
 def markdown_to_html_node(markdown):
     blocks = markdown_to_block(markdown)
     block_type_dict = {
-        "heading": "h",
         "code": "code",
         "quote": "blockquote",
         "unordered_list": "ul",
         "ordered_list": "ol",
         "paragraph": "p"
     }
+    parent_nodes = []
     for block in blocks:
         block_type = block_to_block_type(block)
+        children_nodes = text_to_children(block, block_type)
         if block_type == block_type_heading:
             result = re.search(r"^(#{1,6})", block)
-            block_type_dict["heading"] = f"h{len(result.group())}"
-        # TODO convert blocks to html first
-        print(f"Here is the block type: {block_type}")
+            heading_level = len(result.group())
+            heading_tag = f"h{heading_level}"
+            parent_node = ParentNode(heading_tag, children_nodes)
+        elif block_type == block_type_code:
+            code_node = ParentNode("code", children_nodes)
+            parent_node = ParentNode("pre", [code_node])
+        else:
+            parent_node = ParentNode(block_type_dict[block_type], children_nodes)
 
+        parent_nodes.append(parent_node)
 
-"""
-Order of operations
-1. Split markdown into blocks using markdown_to_block
-  - So I will have markdown blocks like:
-      # Heading 1
+    div = ParentNode("div", parent_nodes)
+    return div
 
-     Then some paragraph text that might have **bold**, *italic* and `code` in it.
+def remove_block_level_md(block, block_type):
+    new_block = ""
+    lines = block.split("\n")
+    if block_type == block_type_heading:
+        new_block = re.sub(r"^#{1,6} ", "", block)
+        return new_block
+    elif block_type == block_type_code:
+        for i in range(len(lines)):
+            if i == 0:
+                lines[i] = lines[i].rstrip("```")
+            elif i == len(lines) - 1:
+                lines[i] = lines[i].lstrip("```")
+    elif block_type == block_type_quote:
+        for i in range(len(lines)):
+            lines[i] = lines[i].lstrip("> ")
+    elif block_type == block_type_unordered_list:
+        for i in range(len(lines)):
+            lines[i] = re.sub(r"^[-|*] ", "", lines[i])
+    elif block_type == block_type_ordered_list:
+        for i in range(len(lines)):
+            lines[i] = re.sub(r"^[0-9]+. ", "", lines[i])
 
-     * Unordered list1
-     * Unordered list2
+    new_block = "\n".join(lines)
+    return new_block
 
-     ```Code block = x ```
-
-2. Loop over each block.
-  - Determine the type of each block using block_to_block_type
-  - Based on the block type, create a new HTMLNode using the HTML class (init requires tag, value, children, and props)
-  - Then assign child HTMLNode objects (i.e., ParentNode and LeafNode) to HTMLNode
-    * Use text_to_textnode(text)
-
-3.
-"""
+def text_to_children(text, block_type):
+    html_nodes = []
+    new_text = remove_block_level_md(text, block_type)
+    text_nodes = text_to_textnodes(text)
+    for node in text_nodes:
+        html_nodes.append(text_node_to_html_node(node))
+    return html_nodes
 
 if __name__ == "__main__":
     block = """
@@ -69,4 +91,10 @@ With more paragraph.
 3. Point *three*
 
     """
-    print(markdown_to_html_node(block))
+blocks = markdown_to_block(block)
+print(f"Here are the blocks: {blocks}")
+for block in blocks:
+    stripped_block = remove_block_level_md(block, block_to_block_type(block))
+    print(stripped_block)
+    children_nodes = text_to_children(stripped_block, block_to_block_type(block))
+    print(f"Children: {children_nodes}")
